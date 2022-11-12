b77a3f52bb05ed5802b60bd7954ed0cb83388b55
Thiago Bonfim <bonfimtm@gmail.com>
2022-11-12 22:41:21 +0100

    Add text input field

--- src/components/ButtonCreate.vue
+++ src/components/ButtonCreate.vue
@@ -1,14 +1,25 @@
 <template>
-    <div class="window button create">
-        Create
+    <div style="display: inline-flex; width: 100%;">
+        <input class="input" />
+        <button class="window button create">
+            Create
+        </button>
     </div>
 </template>
 
 <style scoped>
+.input {
+    width: 256px;
+    height: 26px;
+    margin-right: 8px;
+}
+
 .button.create {
-    width: 192px;
+    width: 128px;
+    height: 32px;
+    padding: 6px 6px;
     text-align: center;
-    font-size: x-large;
+    font-size: large;
     font-weight: bold;
 }
 </style>
