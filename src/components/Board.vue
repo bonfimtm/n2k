df98eb8d7c7757782f926f1b3e14146845258749
Thiago Bonfim <bonfimtm@gmail.com>
2022-11-14 23:28:04 +0100

    Refactor board data

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,49 +1,45 @@
 <script setup>
 import Column from './Column.vue'
 import Card from './Card.vue'
+import { Status, AllStatuses } from './model';
 </script>
 
 <script>
 export default {
   data() {
     return {
-      board: [
-        {
-          title: 'To Do',
-          cards: [
-            { id: 1, text: 'Go to the gym' },
-            { id: 2, text: 'Watch TV' },
-          ],
-        },
-        {
-          title: 'Doing',
-          cards: [
-            { id: 3, text: 'Cook dinner' },
-            { id: 4, text: 'Drink wine' },
-          ],
-        },
-        {
-          title: 'Done',
-          cards: [
-            { id: 5, text: 'Drink coffee' },
-            { id: 6, text: 'Eat breakfast' },
-            { id: 7, text: 'Eat lunch' },
-          ],
-        },
+      cards: [
+        { id: 1, text: 'Go to the gym', status: Status.ToDo },
+        { id: 2, text: 'Watch TV', status: Status.ToDo },
+
+        { id: 3, text: 'Cook dinner', status: Status.Doing },
+        { id: 4, text: 'Drink wine', status: Status.Doing },
+
+        { id: 5, text: 'Drink coffee', status: Status.Done },
+        { id: 6, text: 'Eat breakfast', status: Status.Done },
+        { id: 7, text: 'Eat lunch', status: Status.Done },
       ]
     };
-  }
+  },
+  methods: {
+    cardsFor(column) {
+      return new Map(AllStatuses.map(status => [status, this.cards.filter(card => card.status === status)])).get(column);
+    },
+    remove(card) {
+      console.log(`Removing ${card.text}`);
+    }
+  },
 }
 </script>
 
 <template>
   <div class="board">
-    <Column v-for="column in board">
+    <Column v-for="column in AllStatuses">
       <template #title>
-        {{ column.title }}
+        {{ column }}
       </template>
       <template #content>
-        <Card v-for="card in column.cards">
+        <Card v-for="card in cardsFor(column)">
           {{ card.text }}
         </Card>
       </template>
---
+++ src/components/model.js
@@ -0,0 +1,9 @@
+const Status = {
+  ToDo: "ToDo",
+  Doing: "Doing",
+  Done: "Done",
+};
+
+const AllStatuses = [Status.ToDo, Status.Doing, Status.Done];
+
+export { Status, AllStatuses };
