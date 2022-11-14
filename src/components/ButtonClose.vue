329565e7d00119ee48258acb113bf3394c4a8d3e
Thiago Bonfim <bonfimtm@gmail.com>
2022-11-15 00:20:18 +0100

    Use button tag for close button

--- src/components/ButtonClose.vue
+++ src/components/ButtonClose.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="window button">
+    <button class="window button">
         <div class="meridian">
             <div class="diagonal"></div>
         </div>
-    </div>
+    </button>
 </template>
 
 <style scoped>
@@ -14,17 +14,17 @@
 }
 
 .meridian {
-    height: 16px;
-    width: 2px;
-    margin-left: 7px;
+    height: 10px;
+    width: 1px;
+    margin-left: -.5px;
     background-color: var(--text-color);
     transform: rotate(45deg);
     z-index: 1;
 }
 
 .diagonal {
-    height: 16px;
-    width: 2px;
+    height: 10px;
+    width: 1px;
     background-color: var(--text-color);
     transform: rotate(90deg);
     z-index: 2;
